835baefa889824163f4b095da75588fdf05fae04
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-15 22:55:12 -0700

    Reformatting variable names + added (nonfunctioning) delete button to list items

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-/* 1. As a user, I should be able to type a task into the input field.
-2. As a user, I should be able to click some form of a submit button.
-3. As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
-*/
-
-const form = document.querySelector("#create-task-form");
-form.addEventListener("submit", (e) => {
+document.querySelector("#create-task-form")
+.addEventListener("submit", (e) => {
   e.preventDefault();
-  handleForm(e.target.querySelector("#new-task-description").value);
-});
-});
+  buildList(e.target.querySelector("#new-task-description").value);
+})
+})
 
-function handleForm(form) {
+function buildList(list) {
   const listItem = document.createElement("li");
-  listItem.textContent = form;
+  const deleteBtn = document.createElement("button");
+  listItem.textContent = `${list} `;
+  deleteBtn.textContent = "x";
+  listItem.appendChild(deleteBtn);
   document.querySelector("#tasks").appendChild(listItem);
 }
 
