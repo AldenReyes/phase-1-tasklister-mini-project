d35d9f3df28f527162f4401f56257e3618e4d301
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-16 08:01:35 -0700

    Delete button now removes parent element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-document.querySelector("#create-task-form")
-.addEventListener("submit", (e) => {
+const form = document.querySelector("#create-task-form")
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   buildList(e.target.querySelector("#new-task-description").value);
+  form.reset()
 })
 })
 
 function buildList(list) {
   const listItem = document.createElement("li");
   const deleteBtn = document.createElement("button");
+  deleteBtn.addEventListener("click", handleDelete)
   listItem.textContent = `${list} `;
   deleteBtn.textContent = "x";
   listItem.appendChild(deleteBtn);
   document.querySelector("#tasks").appendChild(listItem);
 }
 
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
