ba3a01af4439dc3ea99e95b36cf687e370a77b72
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-15 20:27:33 -0700

    created two variables that select elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+/* 1. As a user, I should be able to type a task into the input field.
+2. As a user, I should be able to click some form of a submit button.
+3. As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+*/
+
+const formText = document.querySelector("#new-task-description")
+const submitButton = document.querySelector("input[type='submit']")
+
 });
+
+
+const formText = document.querySelector("#new-task-description")
+const submitButton = document.querySelector("input[type='submit']")
