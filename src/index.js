b506f4f2a568f65b12257d768cfd063470dd10e2
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-28 20:18:45 -0700

    add priority options to select

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
+const form = document.querySelector("#create-task-form");
+
 document.addEventListener("DOMContentLoaded", () => {
-const form = document.querySelector("#create-task-form")
-form.addEventListener("submit", (e) => {
+  addFormListener();
+  buildDropdown();
+});
+
+function addFormListener() {
+  form.addEventListener("submit", (e) => {
   e.preventDefault();
   buildList(e.target.querySelector("#new-task-description").value);
-  form.reset()
-})
-})
+  form.reset();
+})};
 
 function buildList(list) {
   const listItem = document.createElement("li");
   const deleteBtn = document.createElement("button");
-  deleteBtn.addEventListener("click", handleDelete)
-  listItem.textContent = `${list} `;
+  deleteBtn.addEventListener("click", handleDelete);
   deleteBtn.textContent = "x";
+  listItem.textContent = `${list} `;
   listItem.appendChild(deleteBtn);
   document.querySelector("#tasks").appendChild(listItem);
 }
 
+function buildDropdown() {
+  const dropdown = document.createElement("select");
+  dropdown.setAttribute("id", "prioritySelect");
+  dropdown.addEventListener("change", handleDropdown);
+  form.appendChild(dropdown);
+  buildOptions();
+}
+
+function buildOptions() {
+  const dropdown = document.getElementById("prioritySelect");
+  const options = ["Low", "Medium", "High"];
+  options.map((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.setAttribute("value", option)
+    optionElement.textContent = option;
+    dropdown.appendChild(optionElement);
+    return optionElement;
+  });
+}
+
 function handleDelete(e) {
   e.target.parentNode.remove();
+}
+
+function handleDropdown(e) {
+  console.log(e)
 }
